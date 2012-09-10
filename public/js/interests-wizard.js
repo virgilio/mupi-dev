58fdb36934bad0ed5ebeb5e638d1403de0968bda
Ana Rute <ninexi@gmail.com>
2012-09-10 17:42:16 -0300

    Updates in interest css and wizard js

--- public/js/interests-wizard.js
+++ public/js/interests-wizard.js
@@ -1,74 +1,56 @@
 jQuery(function() {
     jQuery('.fileupload').fileupload('image');
-    jQuery('.btn_delete').click(function(){
-    	jQuery(this).parent().hide('slow');
-    	
-    	jsRoutes.controllers.Interest.ignoreInterest(jQuery(this).parent().attr('id')).ajax({
-	        success: function(data) {
-	          alert("=)");
-	        },
-	        error: function() {
-	        	alert("=(");
-	        }
-	      });
+    jQuery('.interest_normal > .btn_delete').click(function(){
+        jQuery(this).parent().hide('slow');
+
+        jsRoutes.controllers.Interest.ignoreInterest(jQuery(this).parent().attr('id')).ajax({
+            success: function(data) {
+                alert("=)");
+            },
+            error: function() {
+                alert("=(");
+            }
+        });
     });
 
-    jQuery('.btn_favorite').toggle(function(){
-
-        //                    Shrinks the heart icon
-        jQuery(this).css({
-            'background':'transparent url(jsRoutes.Assets.at(img/favorite-hover-mini.png)) no-repeat'
-        });
-
+    jQuery('.interest_normal > .btn_favorite').live('click', function(){
         //                    Checks the checkbox that refers to this interest
         if(jQuery(this).siblings('input:not(:checked)')){
-            jQuery(this).siblings('input[type=checkbox]').attr('checked', true);
+            jQuery(this).siblings('input[name=interests]').attr('checked', true);
         }
-
-        jQuery(this).siblings('.btn_delete').hide();
 
         //                    Shrinks the image div
         jQuery(this).parent().fadeOut('fast',function(){
             jQuery(this).removeClass('interest_normal').addClass('interest_thumb').appendTo('#chosen_interests').fadeIn()
-            });
-        
-        
-        jsRoutes.controllers.Interest.checkInterest(jQuery(this).parent().attr('id')).ajax({
-	        success: function(data) {
-	          alert("=)");
-	        },
-	        error: function() {
-	        	alert("=(");
-	        }
-	      });
-    }, function(){
-
-        //                    Unchecks the checkbox that refers to this interest
-        if(jQuery(this).siblings('input:checked')){
-            jQuery(this).siblings('input[type=checkbox]').attr('checked', false);
-        }
-
-        jQuery(this).css({
-            'background':'transparent url(jsRoutes.Assets.at(img/favorite.png)) no-repeat'
         });
 
+        jsRoutes.controllers.Interest.checkInterest(jQuery(this).parent().attr('id')).ajax({
+            success: function(data) {
+                alert("=)");
+            },
+            error: function() {
+                alert("=(");
+            }
+        });
+    });
+
+    jQuery('.interest_thumb > .btn_favorite').live('click',function(){
+        //                    Unchecks the checkbox that refers to this interest
+        if(jQuery(this).siblings('input:checked')){
+            jQuery(this).siblings('input[name=interests]').attr('checked', false);
+        }
 
         jQuery(this).parent().fadeOut('fast',function(){
             jQuery(this).removeClass('interest_thumb').addClass('interest_normal').appendTo('#all_interests').fadeIn()
-            });
-
-        jQuery(this).siblings('.btn_delete').show();
-        
-        
-        jsRoutes.controllers.Interest.uncheckInterest(jQuery(this).parent().attr('id')).ajax({
-	        success: function(data) {
-	          alert("=)");
-	        },
-	        error: function() {
-	        	alert("=(");
-	        }
-	      });
-
+        });
     });
-})
+    jsRoutes.controllers.Interest.uncheckInterest(jQuery(this).parent().attr('id')).ajax({
+        success: function(data) {
+            alert("=)");
+        },
+        error: function() {
+            alert("=(");
+        }
+    });
 
+})
