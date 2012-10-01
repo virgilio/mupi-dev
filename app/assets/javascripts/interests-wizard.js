a505adaffb9fb6695c5e6c7aa1dc6c4701266efb
Ana Rute <ninexi@gmail.com>
2012-10-01 14:21:29 -0300

    at interest manager the whole interest is now clickable

--- app/assets/javascripts/interests-wizard.js
+++ app/assets/javascripts/interests-wizard.js
@@ -4,29 +4,29 @@ jQuery(function() {
         jQuery(this).parent().hide('slow');
     });
 
-    jQuery('.interest_normal > .btn_favorite').live('click', function(){
+    jQuery('.interest_normal').live('click', function(){
         // Checks the checkbox that refers to this interest
-    	jsRoutes.controllers.Interest.checkInterest(jQuery(this).parent().attr('id')).ajax(succErrBuilder());
+    	jsRoutes.controllers.Interest.checkInterest(jQuery(this).attr('id')).ajax(succErrBuilder());
 
-        if(jQuery(this).siblings('input:not(:checked)')){
-            jQuery(this).siblings('input[name=interests]').attr('checked', true);
+        if(jQuery(this).children('input:not(:checked)')){
+            jQuery(this).children('input[name=interests]').attr('checked', true);
         }
 
         // Shrinks the image div
-        jQuery(this).parent().fadeOut('fast',function(){
+        jQuery(this).fadeOut('fast',function(){
             jQuery(this).removeClass('interest_normal').addClass('interest_thumb').appendTo('#chosen_interests').fadeIn()
         });
     });
 
-    jQuery('.interest_thumb > .btn_favorite').live('click',function(){
+    jQuery('.interest_thumb').live('click',function(){
     	// Unchecks the checkbox that refers to this interest
-    	jsRoutes.controllers.Interest.uncheckInterest(jQuery(this).parent().attr('id')).ajax(succErrBuilder());
+    	jsRoutes.controllers.Interest.uncheckInterest(jQuery(this).attr('id')).ajax(succErrBuilder());
     	
-    	if(jQuery(this).siblings('input:checked')){
-            jQuery(this).siblings('input[name=interests]').attr('checked', false);
+    	if(jQuery(this).children('input:checked')){
+            jQuery(this).children('input[name=interests]').attr('checked', false);
         }
 
-        jQuery(this).parent().fadeOut('fast',function(){
+        jQuery(this).fadeOut('fast',function(){
             jQuery(this).removeClass('interest_thumb').addClass('interest_normal').appendTo('#all_interests').fadeIn()
         });
     });
