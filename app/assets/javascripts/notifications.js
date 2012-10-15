7b6e7cd91ce79f352ed8fb4df662c179072d036e
Mauricio Banduk <banduk@gmail.com>
2012-10-15 17:12:43 -0300

    fixing clear all notification

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -9,7 +9,18 @@ jQuery(function(){
 		}
 	)
 	jQuery("#clearNotifications").click( function(){
-		jsRoutes.controllers.Mupi.clearBucket().ajax();
+		jsRoutes.controllers.Mupi.clearBucket().ajax({
+			success: function(data) {
+				jQuery("#notification_popup").hide();	
+				
+				jQuery('#notifications').html(
+						"<i class='icon-caret-up caret_not'></i>" +
+						"<ul> <li class='li_empty_notification'>Não há novas notificações</li></ul>"
+				);
+				jQuery("#open_notifications").trigger('click');
+			},
+			error: function(data) {}
+		})
 	})
 })
 
