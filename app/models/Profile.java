6d26d5dc6768f47b5282dbce162bdfc7d11e60bf
Virgílio <virgilio.santos@gmail.com>
2012-10-21 15:53:39 -0200

    adding 'change notification level' support, not yet presented to user. Adding a deleteFromBucket method but yet to discuss delete vs (un)follow

--- app/models/Profile.java
+++ app/models/Profile.java
@@ -24,294 +24,311 @@ import play.db.ebean.Model;
 @Entity
 @Table(name = "profiles")
 public class Profile extends Model {
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
-	private static final String NO_PIC = "/blank.jpg";
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String NO_PIC = "/blank.jpg";
+    
+    @Id
+    private Long id;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    @Formats.DateTime(pattern = "dd/MM/yyyy")
+    private Date birthDate;
+    
+    private String picture = NO_PIC;
 
-	@Id
-	private Long id;
+    @Column(columnDefinition = "TEXT")
+    private String about;
 
-	private String firstName;
+    private Integer gender;
 
-	private String lastName;
+    private Integer status = MupiParams.FIRST_LOGIN;
 
-	@Formats.DateTime(pattern = "dd/MM/yyyy")
-	private Date birthDate;
+    private Integer notificationLevel = MupiParams.NotificationLevel.NORMAL.ordinal();
 
-	private String picture = NO_PIC;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Location> locations = new ArrayList<Location>();
 
-	@Column(columnDefinition = "TEXT")
-	private String about;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Interest> interests = new ArrayList<Interest>();
 
-	private Integer gender;
+    @OneToMany
+    private List<Publication> publications = new ArrayList<Publication>();
 
-	private Integer status = MupiParams.FIRST_LOGIN;
+    @OneToMany
+    private List<PubComment> pubComments = new ArrayList<PubComment>();
 
-	@ManyToMany(cascade = CascadeType.ALL)
-	private List<Location> locations = new ArrayList<Location>();
-
-	@ManyToMany(cascade = CascadeType.ALL)
-	private List<Interest> interests = new ArrayList<Interest>();
-
-	@OneToMany
-	private List<Publication> publications = new ArrayList<Publication>();
-
-	@OneToMany
-	private List<PubComment> pubComments = new ArrayList<PubComment>();
-
-	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date created;
-	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date modified;
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date created;
+    @Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date modified;
 
 
-	public static final Finder<Long, Profile> find = new Finder<Long, Profile>(
-			Long.class, Profile.class);
-
-	public Profile(User user, String firstName, String lastName, Date birthDate, String picture,
-			String about, Integer gender, List<Location> locations) {
-		this.firstName 	= firstName;
+    public static final Finder<Long, Profile> find = new Finder<Long, Profile>(
+                                                                               Long.class, Profile.class);
+    
+    public Profile(User user, String firstName, String lastName, Date birthDate, String picture,
+                   String about, Integer gender, Integer notificationLevel, List<Location> locations) {
+        this.firstName 	= firstName;
         this.lastName 	= lastName;
         this.birthDate 	= birthDate;
         this.picture 	= picture;
         this.about 		= about;
         this.gender 	= gender;
+        this.notificationLevel = notificationLevel;
         this.locations  = locations;
         this.status 	= MupiParams.FIRST_LOGIN;
         this.created	= new Date();
         this.modified	= new Date();
     }
-
-	public Profile() {
-	  this.status = MupiParams.FIRST_LOGIN;
-    this.picture = NO_PIC;
-    this.created = new Date();
-    this.modified = new Date();
-	}
-
-	public Profile(String name) {
-		this.firstName = name;
-		this.status = MupiParams.FIRST_LOGIN;
-		this.picture = NO_PIC;
-		this.created = new Date();
-		this.modified = new Date();
-	}
-
-
-	/**
-	 * Método que cria profile.
-	 * @param authUser
-	 * @return
-	 */
-	public static Profile update(
-			final User user,
-			final String firstName,
-			final String lastName,
-			final String about,
-			final Date birthDate,
-			final String picture,
-			final Integer gender,
-			final List<Location> locations) {
+    
+    public Profile() {
+        this.status = MupiParams.FIRST_LOGIN;
+        this.picture = NO_PIC;
+        this.created = new Date();
+        this.modified = new Date();
+    }
+    
+    public Profile(String name) {
+        this.firstName = name;
+        this.status = MupiParams.FIRST_LOGIN;
+        this.notificationLevel = MupiParams.NotificationLevel.NORMAL.ordinal();
+        this.picture = NO_PIC;
+        this.created = new Date();
+        this.modified = new Date();
+    }
 
 
-		final Profile p = user.profile;
-
-		p.setFirstName(firstName);
-		p.setLastName(lastName);
-		p.setBirthDate(birthDate);
-		p.setPicture(picture);
-		p.setAbout(about);
-		p.setGender(gender);
-		p.setLocations(locations);
-		p.setModified(new Date());
-
-		p.update();
-
-		User.updateName(user, firstName);
-
-		return p;
-	}
-
-	public static Profile changeStatus(final Profile profile, final Integer newStatus){
-	  final Profile p = find.byId(profile.getId());
-	  p.setStatus(newStatus);
-	  p.save();
-		return profile;
-	}
-
-	public static Profile create(final User user) {
-		Profile p = new Profile();
-		p.setFirstName(user.name);
-		p.setStatus(MupiParams.FIRST_LOGIN);
-		p.setPicture(NO_PIC);
-		p.setLocations(new ArrayList<Location>());
-		p.setCreated(new Date());
-		p.setModified(new Date());
+    /**
+     * Método que cria profile.
+     * @param authUser
+     * @return
+     */
+    public static Profile update(
+                                 final User user,
+                                 final String firstName,
+                                 final String lastName,
+                                 final String about,
+                                 final Date birthDate,
+                                 final String picture,
+                                 final Integer gender,
+                                 final Integer notificationLevel,
+                                 final List<Location> locations) {
 
 
-		p.save();
+        final Profile p = user.profile;
+        System.out.println(notificationLevel + ": " + p.getNotificationLevel());
+        
 
-		return p;
-	}
+        p.setFirstName(firstName);
+        p.setLastName(lastName);
+        p.setBirthDate(birthDate);
+        p.setPicture(picture);
+        p.setAbout(about);
+        p.setGender(gender);
+        p.setNotificationLevel(notificationLevel);
+        p.setLocations(locations);
+        p.setModified(new Date());
 
-	public static Profile updateFirstName(final User user, final String name) {
-		final Profile p = user.profile;
-		p.setFirstName(name);
-		p.setModified(new Date());
-		p.update();
-		return p;
-	}
+        p.update();
 
-	public static boolean setLocations(User user, List<Long> locations){
-		Profile profile = user.profile;
-		if(profile != null){
-			profile.setLocations(Location.getLocationsByIds(locations));
-			profile.setModified(new Date());
-			return true;
-		}
-		return false;
-	}
+        User.updateName(user, firstName);
 
-	public String getLocationJsonArray(){
-		String json = "[";
-		for (Location location : this.locations) {
-			json = json.concat("{'id':" + location.getId() + ", text:" + location.getName() + "}");
-		}
-		return json.replace("}{", "},{").concat("]");
-	}
+        return p;
+    }
 
-	public static boolean checkInterest(final User user, final Long interest) {
-		final Profile p = User.findByEmail(user.email).getProfile();
-		if (p != null && p.getInterests().add(models.Interest.find.byId(interest))){
-			p.update();
-			return true;
-		}
-		return false;
-	}
+    public static Profile changeStatus(final Profile profile, final Integer newStatus){
+        final Profile p = find.byId(profile.getId());
+        p.setStatus(newStatus);
+        p.save();
+        return profile;
+    }
 
-	public static boolean uncheckInterest(final User user, final Long interest) {
-		final Profile p = User.findByEmail(user.email).profile;
-		final Interest i = models.Interest.find.byId(interest);
-		if (p != null && i != null && p.getInterests().remove(i)){
-			p.update();
-			return true;
-		}
-		return false;
-	}
+    public static Profile create(final User user) {
+        Profile p = new Profile();
+        p.setFirstName(user.name);
+        p.setStatus(MupiParams.FIRST_LOGIN);
+        p.setPicture(NO_PIC);
+        p.setLocations(new ArrayList<Location>());
+        p.setCreated(new Date());
+        p.setModified(new Date());
 
-	public Long getId() {
-		return id;
-	}
 
-	public Integer getStatus() {
-		return status;
-	}
+        p.save();
 
-	public String getFirstName() {
-		return firstName;
-	}
+        return p;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public static Profile updateFirstName(final User user, final String name) {
+        final Profile p = user.profile;
+        p.setFirstName(name);
+        p.setModified(new Date());
+        p.update();
+        return p;
+    }
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
+    public static boolean setLocations(User user, List<Long> locations){
+        Profile profile = user.profile;
+        if(profile != null){
+            profile.setLocations(Location.getLocationsByIds(locations));
+            profile.setModified(new Date());
+            return true;
+        }
+        return false;
+    }
 
-	public String getPicture() {
-		return picture;
-	}
+    public String getLocationJsonArray(){
+        String json = "[";
+        for (Location location : this.locations) {
+            json = json.concat("{'id':" + location.getId() + ", text:" + location.getName() + "}");
+        }
+        return json.replace("}{", "},{").concat("]");
+    }
 
-	public String getAbout() {
-		return about;
-	}
+    public static boolean checkInterest(final User user, final Long interest) {
+        final Profile p = User.findByEmail(user.email).getProfile();
+        if (p != null && p.getInterests().add(models.Interest.find.byId(interest))){
+            p.update();
+            return true;
+        }
+        return false;
+    }
 
-	public Integer getGender() {
-		return gender;
-	}
+    public static boolean uncheckInterest(final User user, final Long interest) {
+        final Profile p = User.findByEmail(user.email).profile;
+        final Interest i = models.Interest.find.byId(interest);
+        if (p != null && i != null && p.getInterests().remove(i)){
+            p.update();
+            return true;
+        }
+        return false;
+    }
 
-	public List<Location> getLocations() {
-		return locations;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public List<Interest> getInterests() {
-		return interests;
-	}
+    public Integer getStatus() {
+        return status;
+    }
 
-	public List<Publication> getPublications() {
-		return publications;
-	}
+    public Integer getNotificationLevel() {
+        return notificationLevel;
+    }
 
-	public List<PubComment> getPubComments() {
-		return pubComments;
-	}
 
-	public Date getCreated() {
-		return created;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public Date getModified() {
-		return modified;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Date getBirthDate() {
+        return birthDate;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getPicture() {
+        return picture;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public String getAbout() {
+        return about;
+    }
 
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+    public Integer getGender() {
+        return gender;
+    }
 
-	public void setPicture(String picture) {
-		this.picture = picture;
-	}
+    public List<Location> getLocations() {
+        return locations;
+    }
 
-	public void setAbout(String about) {
-		this.about = about;
-	}
+    public List<Interest> getInterests() {
+        return interests;
+    }
 
-	public void setGender(Integer gender) {
-		this.gender = gender;
-	}
+    public List<Publication> getPublications() {
+        return publications;
+    }
 
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+    public List<PubComment> getPubComments() {
+        return pubComments;
+    }
 
-	public void setLocations(List<Location> locations) {
-		this.locations = locations;
-	}
+    public Date getCreated() {
+        return created;
+    }
 
-	public void setInterests(List<Interest> interests) {
-		this.interests = interests;
-	}
+    public Date getModified() {
+        return modified;
+    }
 
-	public void setPublications(List<Publication> publications) {
-		this.publications = publications;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setPubComments(List<PubComment> pubComments) {
-		this.pubComments = pubComments;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setCreated(Date created) {
-		this.created = created;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setModified(Date modified) {
-		this.modified = modified;
-	}
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setNotificationLevel(Integer notificationLevel) {
+        this.notificationLevel = notificationLevel;
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public void setInterests(List<Interest> interests) {
+        this.interests = interests;
+    }
+
+    public void setPublications(List<Publication> publications) {
+        this.publications = publications;
+    }
+
+    public void setPubComments(List<PubComment> pubComments) {
+        this.pubComments = pubComments;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
 
 
 }
