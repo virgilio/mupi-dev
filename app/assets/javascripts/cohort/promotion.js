4fa8d11ab41efc8b37246e9aa160e41a711b111e
Mauricio Banduk <banduk@banduk@mupi.me>
2012-11-09 17:28:56 -0200

    changing parameters for analytics cohort

--- app/assets/javascripts/cohort/promotion.js
+++ app/assets/javascripts/cohort/promotion.js
@@ -3,14 +3,14 @@ jQuery(function() {
 
   Cohorts.Options.debug = true;
   var promotion = new Cohorts.Test({
-    name: 'promotion',
+    name: 'promotion_page_interaction',
     sample: 1, // we want to include all visitors in the test
     cohorts: {
       subscribe:{},
       subscribe_B:{}
     },
     storageAdapter: {
-      nameSpace: 'cohorts',
+      nameSpace: 'promotion',
       trackEvent: function(category, action, opt_label, opt_value) {
         console.log('GA trackEvent: ' + category + ', ' + action + ', ' + opt_label + ', ' + opt_value);
 
@@ -32,6 +32,9 @@ jQuery(function() {
     promotion.event('subscription_click');
   });
 
+  /**
+   * Detecting when the comments div appears to a user
+   */
   var el = jQuery("#promotion_comments")[0].getBoundingClientRect();
   if(el.top >= 0 && el.left >= 0 && el.top <= window.innerHeight && el.left <= window.innerWidth ){
     promotion.event('comments_view');
